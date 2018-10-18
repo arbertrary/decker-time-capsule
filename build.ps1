981156da43423260de953d4b4049a73ecd3c6f49
Jan-Philipp Stauffert <jan-philipp.stauffert@uni-wuerzburg.de>
2018-10-18 14:53:17 +0200

    Fix CI build

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 Write-Output "Building Windows Decker"
-& stack build
 & yarn install
 & yarn run webpack --mode production
 Copy-Item -Recurse -Force node_modules/reveal.js-menu resource/support/
+& stack build
 
 $binpath=(Join-Path ($(stack path | Select-String -Pattern "local-install-root") -split " ")[1] "bin\decker.exe")
 Copy-Item $binpath .
