fd2d701251fa1005cf7c273a3ac3cac2310e9a00
Jan-Philipp Stauffert <jan-philipp.stauffert@uni-wuerzburg.de>
2018-10-18 14:31:33 +0200

    Update CI

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-echo "Building Windows Decker"
+Write-Output "Building Windows Decker"
 & stack build
 & yarn install
 & yarn run webpack --mode production
@@ -6,3 +6,10 @@ Copy-Item -Recurse -Force node_modules/reveal.js-menu resource/support/
 
 $binpath=(Join-Path ($(stack path | Select-String -Pattern "local-install-root") -split " ")[1] "bin\decker.exe")
 Copy-Item $binpath .
+
+$version = Get-Content .\package.yaml | Select-String -Pattern "version: "
+$version = $version -replace "\s+", " "
+$version = ($version -split " ")[1]
+Write-Output $version > version.txt
+
+Compress-Archive -Path .\resource -CompressionLevel Fastest -DestinationPath resource
