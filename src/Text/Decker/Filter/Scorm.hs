e27f7828f8883a7c8b04c35536194266620d5d3e
sam17kd <samantha.monty@uni-wuerzburg.de>
2019-11-07 12:26:50 +0100

    get course info from meta

--- src/Text/Decker/Filter/Scorm.hs
+++ src/Text/Decker/Filter/Scorm.hs
@@ -1,134 +1,136 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-| 
-  Module: Scorm
-  Description: Provides functionality for creating different types of quiz questions
-  Author: Samantha Monty <samantha.monty@uni-wuerzburg.de>
-  
-  This module creates an imsmanifest.xml file to enable SCORM standards used in
-    Learning Management Systems.
+{-# LANGUAGE OverloadedStrings #-}
+{-| 
+  Module: Scorm
+  Description: Provides functionality for creating different types of quiz questions
+  Author: Samantha Monty <samantha.monty@uni-wuerzburg.de>
+  
+  This module creates an imsmanifest.xml file to enable SCORM standards used in
+    Learning Management Systems.
 -}
-module Text.Decker.Filter.Scorm
-( buildScorm
-) where
+module Text.Decker.Filter.Scorm
+( buildScorm
+) where
 
-import Control.Monad (filterM)
-import Control.Monad.Extra (partitionM)
-import qualified Data.ByteString.Lazy as Lazy (putStr, writeFile)
-import Data.List
-import qualified Data.Text as T hiding (isSuffixOf)
--- import qualified Text.XML.Unresolved as XR
-import Data.XML.Types
-import Development.Shake hiding (doesDirectoryExist)
-import System.Directory
-import System.FilePath
-import Text.XML.Unresolved (renderLBS, def)
+import Control.Exception (throw)
+import Control.Monad.Extra (partitionM)
+import qualified Data.ByteString.Lazy as Lazy (writeFile)
+import Data.List
+import qualified Data.Text as T hiding (isSuffixOf)
+import Data.Time.Clock (getCurrentTime, utctDay)
+import Data.XML.Types
+import Data.Yaml
+import Development.Shake hiding (doesDirectoryExist)
+import System.Directory
+import System.FilePath
+import Text.Decker.Internal.Common (pandocReaderOpts)
+-- import Text.Decker.Project.Project
+import Text.Pandoc.Class (runPure)
+import Text.Pandoc.Definition
+import Text.Pandoc.Error
+import Text.Pandoc.Readers (readMarkdown)
+import Text.Pandoc.Shared (stringify)
+import Text.XML.Unresolved (renderLBS, def)
 
--- import Data.Yaml
-import Text.Decker.Project.Project
--- import Text.XML.Generator
+-- Defined in the YAML header
+data Course = Course 
+    { name :: String
+    , identifier :: String
+    , version :: String
+    } deriving Show
+    
+getFile :: String -> [FilePath] -> FilePath
+getFile target (file:fileList) = 
+    case target `isSuffixOf` file of 
+        True -> file
+        False -> getFile target fileList
+getFile target [] = error $ "Can't find " ++ target 
 
--- Defined in the YAML header
-data Course = Course 
-    { name :: String
-    , identifier :: String
-    , version :: String
-    , lessonTitle :: String
-    } deriving Show
-
-
-    -- nameOfCourse = name <$> Course
-
--- Search Project dir for MD file
-    -- read YMAL to Course data
-    -- getCourseInfo :: FilePath -> IO Course
-    -- getCourseInfo projDir = do
-    --     let mdFile = 
-    --     course <- XR.readfile mdFile
-    --     return $ course
-
-    -- Gets course info from YAML header, need path of markdown file
-    -- getCourse :: FilePath -> Pandoc -> Course
-    -- getCourse mdFile = do
-    --     markdown <- T.readFile mdFile
-    --     let Pandoc metadata content = case runPure (readMarkdown _ markdown) of
-    --             Right pandoc -> pandoc
-    --             Left errMsg -> throw $ PandocException (show errMsg)
-    --     let lessonName = maybe [] (map text) $ getField "title" metadata
-    --     let courseName = maybe [] (map text) $ getField "course-name" metadata
-    --     let courseId = maybe [] (map text) $ getField "course-id" metadata
-    --     let courseVersion = maybe [] (map text) $ getField "course-version" metadata
-    --     return $ Course courseName courseId courseVersion lessonName
+getYaml :: String -> Meta -> IO String   
+getYaml field meta = do
+    case lookupMeta field meta of 
+        Just (MetaInlines s) -> return $ stringify s
+        Nothing -> throw $ YamlException yamlWarning
+    where
+        yamlWarning = "The YAML header of your markdown file requires title, author and version."
+        
+-- Gets course info from YAML header
+getCourse :: FilePath -> IO Course
+getCourse projDir = do
+    names <- listDirectory projDir
+    markdownFile <- readFile $ getFile "-deck.md" names
+    let markdown = T.pack markdownFile
+    case runPure (readMarkdown pandocReaderOpts markdown) of
+        Right pandoc@(Pandoc meta _) -> do
+            title <- getYaml "title" meta
+            author <- getYaml "author" meta 
+            version <- getYaml "version" meta 
+            date <- getCurrentTime >>= return . show . utctDay
+            let identifier = (take 3 title) ++ "_" ++ (take 3 author) ++ date 
+            return $ Course title identifier version 
+        Left errMsg -> throw $ PandocParseError (show errMsg)
   
-metaData :: Node 
-metaData = NodeElement $ Element "metadata" [] [schema, schemaVersion]
+listFiles :: FilePath -> IO [FilePath]
+listFiles dir = do
+    names <- listDirectory dir
+    let qualifiedNames = map (dir </>) names
+    (dirs, files) <- partitionM System.Directory.doesDirectoryExist qualifiedNames   -- files returns support dir
+    subFiles <- mconcat $ map listFiles dirs
+    let allFiles = files ++ subFiles
+    let noStores = dropWhile (\x -> x == ".DS_Store") allFiles 
+    return $ map takeFileName noStores
+    
+buildFileTags :: FilePath -> IO [Node]
+buildFileTags dir = do
+    allDirs <- listFiles dir
+    return $ fmap wrapTag allDirs
+    where
+        wrapTag file = NodeElement $ Element "file" [("href", [ContentText $ T.pack file])] []
+
+buildResources :: FilePath -> T.Text -> IO Node
+buildResources pubDir courseID = do 
+    pubFiles <- listDirectory pubDir
+    let resAttrs =  [("identifier", [ContentText courseID])
+                    ,("type", [ContentText "webcontent"])
+                    ,("href", [ContentText $ T.pack $ getFile "-deck.html" pubFiles])
+                    ,("adlcp:scormtype", [ContentText "sco"])]
+    resChildren <- buildFileTags pubDir   
+    let children = NodeElement $ Element "resource" resAttrs resChildren          
+    return $ NodeElement $ Element "resources" [] [children]
+
+metaData :: Node 
+metaData = NodeElement $ Element "metadata" [] [schema, schemaVersion]
+    where
+        schema = NodeElement $ Element "schema" [] [NodeContent $ ContentText $ T.pack "ADL SCORM"]
+        schemaVersion = NodeElement $ Element "schemaversion" [] [NodeContent $ ContentText $ T.pack "1.2"]
+
+buildOrg :: Course -> Node
+buildOrg course = NodeElement $ Element "organizations" attrs [org]
     where
-        schema = NodeElement $ Element "schema" [] [NodeContent $ ContentText $ T.pack "ADL SCORM"]
-        schemaVersion = NodeElement $ Element "schemaversion" [] [NodeContent $ ContentText $ T.pack "1.2"]
+        courseName = NodeContent $ ContentText $ T.pack $ name course
+        resId = ContentText $ T.pack $ identifier course ++ "_RES"
+        orgId = ContentText $ T.pack $ identifier course ++ "_ORG"
+        itemId = ContentText $ T.pack $ identifier course ++ "_ITEM"
+        attrs = [("identifier", [orgId])]
+        title = NodeElement $ Element "title" [] [courseName] 
+        itemAttrs = [ ("identifier", [itemId])
+                    , ("isvisible", [ContentText "true"])
+                    , ("identifierref", [resId])]
+        itemChildren = [NodeElement $ Element "title" [] [courseName]]
+        item = NodeElement $ Element "item" itemAttrs itemChildren
+        org = NodeElement $ Element "organization" attrs [title, item]  
 
-        -- <organization identifier="GOL_23.10.19_ORG">
+writeManifest :: FilePath -> FilePath -> IO ()
+writeManifest projDir pubDir = do
+    course <- getCourse projDir  
+    let courseID = T.pack $ identifier course
+    let courseVersion = T.pack $ version course
+    let orgs = buildOrg course
+    resources <- buildResources pubDir courseID
+    let attrs = [ ("identifier", [ContentText $ courseID])
+                , ("version", [ContentText $ courseVersion])]
+    let root = Element "manifest" attrs [metaData, orgs, resources]
+    let manifestDoc = renderLBS def (Document (Prologue [] Nothing []) root [])
+    Lazy.writeFile (pubDir </> "imsmanifest.xml") manifestDoc
 
-orgs :: Node
-orgs = NodeElement $ Element "organizations" attrs [org]
-    where
-        id = "course-identifier" ++ "_ORG"
-        attrs = [("identifier", [ContentText $ T.pack id])]
-        title = NodeElement $ Element "title" [] [NodeContent $ ContentText $ T.pack "course-name"] 
-        itemId = "course-identifier" ++ "_item"
-        itemIdRef = "course-identifier" ++ "_res"
-        itemAttrs = [ ("identifier", [ContentText $ T.pack itemId])
-                    , ("isvisible", [ContentText "true"])
-                    , ("identifierref", [ContentText $ T.pack itemIdRef])]
-        itemChildren = [NodeElement $ Element "title" [] [NodeContent $ ContentText $ T.pack "lesson-title"]]
-        item = NodeElement $ Element "item" itemAttrs itemChildren
-        org = NodeElement $ Element "organization" attrs [title, item]
-
-getHtml :: [FilePath] -> FilePath
-getHtml (file:fileList) = do
-    let isHtml = "-deck.html" `isSuffixOf` file
-    case isHtml of 
-        True -> file
-        False -> getHtml fileList
-getHtml [] = error $ "Cant find *-deck.html" 
-
--- lists all directories in a directory, returns a list of all directories
-getFiles :: FilePath -> IO [FilePath]
-getFiles dir = do
-    names <- listDirectory dir
-    (dirs, files) <- partitionM System.Directory.doesDirectoryExist names   -- files returns support dirmm
-    subFiles <- mconcat $ map getFiles dirs
-    let allFiles = files ++ subFiles
-    return $ dropWhile (\x -> x == "support" || x == ".DS_Store") allFiles
-    -- listDirectory dir >>= filterM (doesDirectoryExist . (++) dir )
-
--- Expected type is return type in signature
-buildFileTags :: FilePath -> IO [Node]
-buildFileTags dir = do
-    allDirs <- getFiles dir
-    return $ fmap wrapTag allDirs
-    where
-        wrapTag file = NodeElement $ Element "file" [("href", [ContentText $ T.pack file])] []
-
-buildResources :: FilePath -> IO Node
-buildResources pubDir = do 
-    pubFiles <- listDirectory pubDir
-    let resAttrs =  [("identifier", [ContentText "course-identifier_res"])
-                    ,("type", [ContentText "webcontent"])
-                    ,("href", [ContentText $ T.pack $ getHtml pubFiles])
-                    ,("adlcp:scormtype", [ContentText "sco"])]
-    resChildren <- buildFileTags pubDir   
-    let children = NodeElement $ Element "resource" resAttrs resChildren          
-    return $ NodeElement $ Element "resources" [] [children]
-
-writeManifest :: FilePath -> FilePath -> IO ()
-writeManifest projDir pubDir = do
-    -- get courseInfo from markdown YAML
-    -- course <- getCourseInfo projDir  
-    resources <- buildResources pubDir
-    let attrs = [ ("identifier", [ContentText "course-identifier"])
-                , ("version", [ContentText "course-version"])]
-    let root = Element "manifest" attrs [metaData, orgs, resources]
-    let manifestDoc = renderLBS def (Document (Prologue [] Nothing []) root [])
-    Lazy.writeFile (pubDir </> "imsmanifest.xml") manifestDoc
-
-buildScorm :: FilePath -> FilePath -> Action ()
-buildScorm proj pub = runAfter $ writeManifest proj pub
+buildScorm :: FilePath -> FilePath -> Action ()
+buildScorm proj pub = runAfter $ writeManifest proj pub
