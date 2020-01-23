1d1d9848f4c0c134074588420de74714aa88c2fa
Armin Bernstetter <armin.bernstetter@uni-wuerzburg.de>
2020-01-23 11:19:46 +0100

    added -local option to build.ps1. copies decker exe to ProgramFiles (x86)\Decker\bin

--- bin/build.ps1
+++ bin/build.ps1
@@ -7,7 +7,8 @@
 #>
 Param(
     [switch] $skiptemplates,
-    [switch] $preparepackage
+    [switch] $preparepackage,
+    [switch] $local
 )
 Write-Output "Building Windows Decker"
 if (-Not $skiptemplates) {
@@ -31,4 +32,22 @@ if ($preparepackage) {
     $version = $version -replace "\s+", " "
     $version = ($version -split " ")[1]
     Write-Output $version > version.txt
+}
+
+# If it's a local install, copy the program Decker to C:\ProgramFiles (x86)\Decker
+# C:\ProgramFiles (x86)\bin needs to be on PATH to execute decker from anywhere
+# needs Admin Rights
+if ($local) {
+    $exepath = (Join-Path ($(stack path | Select-String -Pattern "local-install-root") -split " ")[1] "bin\decker.exe")
+
+    $deckerpath = "${Env:ProgramFiles(x86)}\Decker"
+    
+    New-Item -Path "$deckerpath\bin" -Force -ItemType "directory"
+
+    Copy-Item "$exepath" "$binpath\decker.exe"
+
+    $version = Get-Content .\package.yaml | Select-String -Pattern "version: "
+    $version = $version -replace "\s+", " "
+    $version = ($version -split " ")[1]
+    Write-Output $version > "$deckerpath\version.txt"
 }
