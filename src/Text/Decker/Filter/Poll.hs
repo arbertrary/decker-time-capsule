77433382628049c9c1ed997b91d3e325d068d776
Samantha Monty <samantha.monty@uni-wuerzburg.de>
2021-12-17 15:47:54 +0100

    update polls remove blaze

--- src/Text/Decker/Filter/Poll.hs
+++ src/Text/Decker/Filter/Poll.hs
@@ -5,22 +5,17 @@ module Text.Decker.Filter.Poll (handlePolls) where
 
 import Data.Aeson
 import Data.Aeson.TH (deriveJSON)
-import Data.ByteString.Lazy (toStrict)
+import Data.ByteString.Lazy.Internal as L
 import qualified Data.Text as T
-import Data.Text.Encoding as E (encodeUtf8)
+import Data.Text.Encoding as E 
 import Data.Yaml as Y (decodeEither')
-import Text.Blaze.Html
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
-import Text.Decker.Filter.Local (RawHtml (rawHtml'))
-import Text.Decker.Filter.Slide
-  ( Slide (Slide),
-    fromSlides,
-    toSlides,
-  )
+import Text.Decker.Filter.Slide (tag)
 import Text.Decker.Internal.Common (Decker)
 import Text.Decker.Internal.Meta as M
+    ( toPandocMeta, lookupMetaOrElse, lookupMeta )
 import Text.Pandoc.Definition
+import Text.Pandoc.Walk
+
 
 data PollMeta = PollMeta
   { color :: String,
@@ -66,33 +61,28 @@ newtype Axes = Axes { ticks :: Ticks } deriving (Show)
 newtype Legend = Legend Disp deriving (Show)
 newtype Disp = Disp { display :: Bool } deriving (Show)
 
--- Look in YAML for poll:true to see if deck has poll
+-- Look in YAML for poll:true to see if deck has poll, 
+-- then look in slides for 'poll' to parse poll slides  
 handlePolls :: Pandoc -> Decker Pandoc
 handlePolls pandoc@(Pandoc meta blocks) =
   case (M.lookupMeta "poll" meta :: Maybe MetaValue) of
-    Just poll -> return $ Pandoc meta $ fromSlides $ parseSlides $ toSlides blocks
+    Just poll -> return $ walk parseBlocks pandoc
     _ -> return pandoc
-
--- Look through deck for poll response polls (.poll in H1)
-parseSlides :: [Slide] -> [Slide]
-parseSlides (sl@(Slide (Just (Header _ (_, tgs, _) _)) _ _) : slides) =
-  if "poll" `elem` tgs
-    then buildPoll sl : parseSlides slides
-    else sl : parseSlides slides
-parseSlides (s : sl) = parseSlides sl
-parseSlides [] = []
-
--- Build and attach results slide
-buildPoll :: Slide -> Slide
-buildPoll (Slide (Just (Header l (i, t, k) title)) body dir) =
-  Slide (Just (Header l (i, t, k) (icon : title))) (timer : chart : body) dir
   where
-    pm = buildPollMeta $ getYaml body
-    ti = if timed pm then T.pack "timed" else ""
-    timer = Div ("", ["countdown", ti], [("data-seconds", T.pack $ seconds pm)]) []
-    icon = RawInline (Format "html") "<i class=\"fas fa-qrcode\"></i>"
-    chart = renderCanvas (findQuestions body) pm
-buildPoll s = s
+    parseBlocks :: Block -> Block
+    parseBlocks (Div a@(id_, cls, kvs) blocks) 
+      | "poll" `elem` cls = Div a (timer : parsed ++ [chart])
+        where
+          parsed = walk parsePolls blocks
+          pm = buildPollMeta $ getYaml blocks
+          ti = if timed pm then T.pack "timed" else ""
+          timer = Div ("", ["countdown", ti], [("data-seconds", T.pack $ seconds pm)]) []
+          chart = renderCanvas (findQuestions blocks) pm
+    parseBlocks bl = bl
+    parsePolls :: Block -> Block
+    parsePolls (Header 1 a title) = 
+      Header 1 a (RawInline (Format "html") "<i class=\"fas fa-qrcode\"></i>" : title)
+    parsePolls b = b
 
 buildPollMeta :: Maybe Meta -> PollMeta
 buildPollMeta meta = case meta of
@@ -132,7 +122,7 @@ buildMeta [] = Nothing
 
 -- Parse slide body to find question block
 findQuestions :: [Block] -> [String]
-findQuestions (poll@(Div (i, tgs, k) qns) : body) =
+findQuestions ((Div (i, tgs, k) qns) : body) =
   if any (`elem` tgs) ["qmc", "quiz-mc", "quiz-multiple-choice"]
     then parseAnswers qns
     else findQuestions body
@@ -162,17 +152,16 @@ buildAnswers block =
 
 -- Build canvas tag with chart comment to render results of polls
 renderCanvas :: [String] -> PollMeta -> Block
-renderCanvas answers pm =
-  rawHtml' $
-    H.div ! A.class_ "poll_results" $
-      H.canvas ! A.class_ "stretch"
-        ! H.dataAttribute "chart" "bar"
-        $ unsafeByteStringComment $
-          toStrict $ Data.Aeson.encode chartObj
+renderCanvas answers pm = Div ("", ["poll_results"], []) [Plain [canvas]]
   where
+    canvas = tag "canvas" $ Span ("", ["stretch"], [("data-chart", "bar")]) [open,ch,end]
+    open = RawInline "html" $ T.pack "<!-- "
+    ch = RawInline "html" $ T.pack chart
+    end = RawInline "html" $ T.pack " -->"
+    chart = unpackChars $ Data.Aeson.encode chartObj
     dataset = Dataset (color pm) (map (const 0) answers)
-    da = DataObj answers [dataset]
-    chartObj = Chart da (options pm)
+    dataO = DataObj answers [dataset]
+    chartObj = Chart dataO (options pm)
 
 deriveJSON defaultOptions ''PollMeta
 deriveJSON defaultOptions {fieldLabelModifier = drop 5} ''Chart
